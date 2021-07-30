5b2e7febba53846128a61cfb3fbb90d264696776
Jamie Phillips <phillipsja97@gmail.com>
2021-07-30 17:10:44 -0500

    beautiful soup set up to parse the html returned from requests

--- get_transcript.py
+++ get_transcript.py
@@ -1,4 +1,8 @@
 import requests
+from bs4 import BeautifulSoup
 
-req = requests.get("https://www.ted.com/talks/{mariana_atencio_what_makes_you_special}/transcript")
-print(req.text)
+req = requests.get("https://www.ted.com/talks/mariana_atencio_what_makes_you_special/transcript")
+text = req.text
+
+soup = BeautifulSoup(text, 'html.parser')
+print(soup.p)
