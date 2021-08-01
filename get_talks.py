3bce84cef4c34e64de136ac3c2fbb6cdfdf82dc1
Jamie Phillips <phillipsja97@gmail.com>
2021-07-31 23:02:11 -0500

    extracted the img src, author name and video links with beautifulsoup

--- get_talks.py
+++ get_talks.py
@@ -6,21 +6,23 @@ url = "https://www.ted.com/talks?page={sys.argv[1]}"
 req = requests.get(url)
 text = req.text
 
-page = BeautifulSoup(text, 'html.parser')
-# all_links = [a['href'] for a in page.find_all('a', attrs={"data-ga-context":"talks"} , href=True)]
-# needed_links = []
-# for idx, item in enumerate(all_links):
-#     print(item)
-#     if idx % 2 == 0:
-#       needed_links.append(item)
-
-
 soup = BeautifulSoup(text, 'html.parser')
 links_with_text = []
+authors = []
+imagesource = []
 # for a in soup.find_all('a', attrs={"data-ga-context":"talks"}, href=True): 
 for a in soup.find_all(class_="talk-link"): 
-    if a.text:
-      link = a.findNext('a', attrs={"data-ga-context":"talks"}, href=True)
-      links_with_text.append(link['href'])
+  link = a.findNext('a', attrs={"data-ga-context":"talks"}, href=True)
+  links_with_text.append(link['href'])
 
+names = soup.find_all(class_="h12 talk-link__speaker")
+for child in names:
+  authors.append(child.string)
+
+images = soup.findAll('img')
+for image in images:
+  imagesource.append(image['src'])
+
+print(authors)
 print(links_with_text)
+print(imagesource)
